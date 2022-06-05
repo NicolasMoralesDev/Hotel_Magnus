6f93d848532a204e21c1da7a9052c752b4da2f88
Nicolas <nicolasmoralesj03@gmail.com>
2022-06-05 00:18:24 -0300

    mejoras en el spinner load

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onload= function(){
+    let spinner = document.getElementById('spinner');
+    
+    setTimeout(() => {
+       spinner.style.visibility='hidden';
+       spinner.style.opacity='0'; 
+       
+    }, 5000);
+    
+
+};
